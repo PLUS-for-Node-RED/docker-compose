21973ca1f7ed6c231b03f2492ed12bf461903fce
biancode <klaus@bianco-royal.de>
2023-04-15 13:41:33 +0200

    feat(p4nr): flowforge images

--- build-containers.sh
+++ build-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build flowforge-docker -t flowforge/forge-docker
-docker build node-red-container -t flowforge/node-red
+docker build node-red-container -t flowforge/node-red --build-arg P4NR_LICENSE_TOKEN=$P4NR_LICENSE_TOKEN
 docker build file-server -t flowforge/file-server
